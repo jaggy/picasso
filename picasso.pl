0a275af18c67e72a5be64e2ec3d2dacaa5147a76
Jaggy Gauran <jaggygauran@gmail.com>
2014-04-13 22:43:07 +0800

    fix multi match on the single line

--- picasso.pl
+++ picasso.pl
@@ -3,11 +3,12 @@ use warnings;
 use strict;
 use Term::ANSIColor;
 
-my $reset = color('reset');
+my $matchCount = 5;
+my $reset      = color('reset');
 
 while(<>) {
 
-    my (@matches) = ($_ =~ m/(<(black|red|green|yellow|blue|magenta|cyan|white)>)(.*?)(<\/(black|red|green|yellow|blue|magenta|cyan|white)>)/);
+    my (@matches) = ($_ =~ m/(<(black|red|green|yellow|blue|magenta|cyan|white)>)(.*?)(<\/(black|red|green|yellow|blue|magenta|cyan|white)>)/g);
 
     if( !@matches )
     {
@@ -15,14 +16,29 @@ while(<>) {
       next;
     }
 
-    my $opening = $matches[0];
-    my $color   = color($matches[1]);
-    my $closing = $matches[3];
+
+    my $loops = scalar @matches / $matchCount;
+
+    my $start = 0;
+    my $end   = 4;
+
+    while($loops--) {
+        # print $loops, "\n";
+        my @slice   = @matches[$start..$end];
+        my $opening = $slice[0];
+        my $color   = color($slice[1]);
+        my $closing = $slice[3];
+
+        $_ =~ s/$opening/$color/g;
+        $_ =~ s/$closing/$reset/g;
 
 
-    $_ =~ s/$opening/$color/g;
-    $_ =~ s/$closing/$reset/g;
+        $start += $matchCount;
+        $end   += $matchCount;
+    }
+
     print $_;
+
 }
 
 
